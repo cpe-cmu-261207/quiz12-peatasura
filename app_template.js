54442c28a9419a6a08960437ab2028fc8ec4ba2c
peatasura <keenphantom.2013@gmail.com>
2021-01-12 13:58:23 +0700

    555

--- app_template.js
+++ app_template.js
@@ -1,1 +1,14 @@
+let id = document.getElementById("input-number");
+let Run =document.getElementById("button-compute");
+let uu = document.getElementById("compute-result");
+var ids = Number(620610796)
+function calculate(){
+
+     uu =  ids+Number(document.getElementById("input-number").innerHTML);
+}
+if(Run){
+    Run.addEventListener('click',calculate);
+
+  }
+
 //your code here!
